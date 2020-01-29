0f3d8299fb13a0401c00252a3332a6edec0d1fe3
Ignacio Martín <natx.mc@gmail.com>
2020-01-29 12:35:54 +0100

    New PSInstance function to create object with pub/sub capabilities

--- src/js/pubsub.js
+++ src/js/pubsub.js
@@ -4,3 +4,30 @@ export function psTransform(topicIn, topicOut) {
     ps.subscribe(topicIn, (msg, data) => ps.publish(topicOut, data));
 }
 export var ps = PS;
+
+let id = 0;
+
+const psProto = {
+
+    subscribe: function(topic, f) {
+        ps.subscribe(this.id + '.' + topic, f)
+    },
+
+    publish: function(topic, data) {
+        ps.publish(this.id + '.' + topic, data)
+    }
+
+}
+
+export function PSInstance() {
+
+    let internalId = 'psi_' + id++;
+
+    let o = Object.create(psProto);
+
+    Object.defineProperty(o, 'id', {
+        get: () => internalId
+    });
+
+    return o;
+}
