803079f833bf8eef828d6746cd333ff1645ca2fd
Ignacio Martín <natx.mc@gmail.com>
2020-01-29 12:34:50 +0100

    Deal with error in get() promise

--- src/js/views/pages/home.js
+++ src/js/views/pages/home.js
@@ -3,10 +3,16 @@ import Header from '../components/header';
 import Errors from '../components/errors';
 import OrderListView from '../components/order/orderList';
 import { LocalState } from '../../state';
+import {ps} from '../../pubsub';
 
 const Home = {};
 
-Home.oninit = () => LocalState.orders.get();
+Home.oninit = () => LocalState.orders.get().catch(function(e) {
+    ps.publish('error.' + LocalState.orders.id, {
+        show: true,
+        data: new Error('Could not load order list')
+    });
+});
 
 Home.view = function () {
     return m('div', [
