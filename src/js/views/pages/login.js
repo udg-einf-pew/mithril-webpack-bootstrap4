65b7d53cc7a42a95c1eed78cf72ee7c2083bb105
Ignacio Martín <natx.mc@gmail.com>
2020-03-03 12:03:49 +0100

    Add Header to page

--- src/js/views/pages/login.js
+++ src/js/views/pages/login.js
@@ -1,9 +1,11 @@
 import m from "mithril";
 import UserLogin from "../components/user/login";
 import Errors from "../components/errors";
+import Header from '../components/header';
 
 const Login = {
     view: () => m('div', [
+        m(Header),
         m(UserLogin, {loginTopic: 'view.login.request'}),
         m(Errors, { errorTopic: 'error'})
     ])
--- src/js/views/pages/signup.js
+++ src/js/views/pages/signup.js
@@ -1,9 +1,11 @@
 import m from "mithril";
 import UserSignup from "../components/user/signup";
 import Errors from "../components/errors";
+import Header from '../components/header';
 
 const Signup = {
     view: () => m('div', [
+        m(Header),
         m(UserSignup, {signupTopic: 'view.signup.request'}),
         m(Errors, { errorTopic: 'error'})
     ])
