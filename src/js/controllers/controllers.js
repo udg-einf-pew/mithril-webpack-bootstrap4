bfae6a3f35982b7f1e173dc3c218578f54f1978e
Ignacio Martín <natx.mc@gmail.com>
2021-02-28 13:04:20 +0100

    No need to export anything

--- src/js/controllers/controllers.js
+++ src/js/controllers/controllers.js
@@ -1,5 +1,3 @@
+// This is a convenience module to import ALL controllers in one step
+// IMPORTANT: any new controller must be imported here
 import Auth from "./auth";
-
-const Controllers = {};
-
-export default Controllers;
