9c79a7839f8cf94bd0bdfd80178946359a469908
Ignacio Martín <natx.mc@gmail.com>
2021-02-28 13:03:33 +0100

    Model completely refactored

--- src/js/models/model.js
+++ src/js/models/model.js
@@ -1,16 +1,80 @@
-import Api from '../controllers/api/base';
+import Api from '../api';
+import {PSInstance} from '../pubsub';
 
-const proto = {
-    get: function(params) { return Api.get(this.url, params); },
-    getById: function(id) { return Api.get(this.url + '/' + id); },
-    create: function(data) { return Api.post(this.url, data); },
-    delete: function(id) { return Api.delete(this.url + '/' + id); }
-}
 
 function Model(url, name) {
-    let model = Object.create(proto);
-    model.url = url;
-    model.name = name;
+
+    let modelURL = (' ' + url).slice(1);
+    let modelName = (' ' + name).slice(1);
+
+    function load(id) {
+        let that = this;
+        return Api.get(modelURL + '/' + id)
+            .then(function (result) {
+                that.data = result;
+                Object.freeze(that.data);
+                that.publish('update');
+                return that;
+            });
+    }
+
+    function save() {
+        let that = this;
+        if (that.data.id)
+            return Api.put(modelURL + '/' + this.data.id, this.data)
+                .then(function (result) {
+                    that.data = result;
+                    Object.freeze(that.data);
+                    that.publish('save');
+                    return that;
+                });
+        else
+            return Api.post(modelURL, this.data)
+                .then(function (result) {
+                    that.data = result;
+                    Object.freeze(that.data);
+                    that.publish('create');
+                    return that;
+                });
+    }
+
+    function remove() {
+        let that = this;
+        return Api.delete(modelURL + '/' + this.data.id)
+            .then(function (result) {
+                that.publish('remove');
+                return null;
+            });
+    }
+
+    function update(data) {
+        this.data = {...this.data, ...data};
+        Object.freeze(this.data);
+        this.publish('update');
+    }
+
+    let model = function () {
+        let c = Object.create(PSInstance());
+
+        c.data = {};
+        Object.freeze(c.data);
+
+        c.load = load;
+        c.save = save;
+        c.remove = remove;
+        c.update = update;
+
+        return c;
+    }
+
+    Object.defineProperty(model, 'url', {
+        get: () => modelURL
+    });
+
+    Object.defineProperty(model, 'name', {
+        get: () => modelName
+    });
+
     return model;
 }
 
