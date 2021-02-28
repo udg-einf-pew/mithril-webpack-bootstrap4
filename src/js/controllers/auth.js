79c9b04f2c4e3c961afd065337a6d68661f9b537
Ignacio Martín <natx.mc@gmail.com>
2021-02-28 13:02:57 +0100

    Now this is the full controller for authentication

--- src/js/controllers/auth.js
+++ src/js/controllers/auth.js
@@ -1,8 +1,34 @@
-import {psTransform} from '../pubsub';
+import m from 'mithril';
+import {ps, psTransform} from '../pubsub';
+import {LocalStorage} from '../state';
+import Api from '../api';
 
-const Auth = {};
+
+ps.subscribe('api.login.request', function (msg, data) {
+    Api.login({username: data.username, password: data.password})
+        .then(ps.publish.bind(ps, 'api.login.successful'))
+        .catch((e) => ps.publish('error.api.login', {
+            data: new Error(e.response ? e.response.error.message : "Error connecting to server"),
+            show: true
+        }))
+});
+
+ps.subscribe('api.login.successful', function (msg, data) {
+    LocalStorage.setItem('user', data);
+    m.route.set('/');
+});
+
+// Signup
+
+ps.subscribe('api.signup.request', function (msg, data) {
+    Api.signup(data)
+        .then((data) => ps.publish('api.signup.successful', data))
+        .catch(function (error) {
+            ps.publish('error.api.signup', error)
+        })
+});
+
+ps.subscribe('api.signup.successful', () => m.route.set('/login'));
 
 psTransform('view.login.request', 'api.login.request');
 psTransform('view.signup.request', 'api.signup.request');
-
-export default Auth;
